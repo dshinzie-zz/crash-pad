c2837bbe2de1ae725b11657d35b10a02611ffec3
Brad <bgreen3@uoregon.edu>
2016-12-14 20:06:18 -0700

    Add api key generator for all created users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
+require 'securerandom'
+
 class User < ApplicationRecord
+
+  before_create :set_api_key
+
+  private
+
+  def set_api_key
+    return if api_key.present?
+    self.api_key = generate_api_key
+  end
+
+  def generate_api_key
+    SecureRandom.uuid.gsub(/\-/,'')
+  end
+
 end

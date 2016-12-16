0e0a193b0a4221a85092f3df808c5a8a82caad92
Chase Dunagan <cdunagan@utexas.edu>
2016-12-15 18:36:43 -0700

    add edit profile and login to navbar

--- spec/features/dashboard/logged_in_user_can_see_dashboard_spec.rb
+++ spec/features/dashboard/logged_in_user_can_see_dashboard_spec.rb
@@ -22,7 +22,53 @@ describe "As a logged in user" do
       click_on "Dashboard"
       click_on "Edit Profile"
 
-      expect(current_path).to eq(edit_user_path)
+      expect(current_path).to eq(edit_user_path(user))
+    end
+
+    it "allows me to edit profile" do
+      user = stub_login_user
+
+      visit root_path
+      click_on "Dashboard"
+      click_on "Edit Profile"
+
+      expect(current_path).to eq(edit_user_path(user))
+
+      fill_in 'Email', with: "johnnyquest@test.com"
+      fill_in 'First Name', with: "Johnny"
+      fill_in 'Last Name', with: "Quest"
+      fill_in 'Phone Number', with: "999-999-9999"
+
+      click_on "Update Account"
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("johnnyquest@test.com")
+      expect(page).to have_content("Johnny")
+      expect(page).to have_content("Quest")
+      expect(page).to have_content("999-999-9999")
+
+      expect(page).to_not have_content(user.email)
+      expect(page).to_not have_content(user.first_name)
+      expect(page).to_not have_content(user.last_name)
+      expect(page).to_not have_content(user.phone)
+
+    end
+
+    it "allows me to edit profile" do
+      user = stub_login_user
+
+      visit root_path
+      click_on "Dashboard"
+      click_on "Edit Profile"
+
+      expect(current_path).to eq(edit_user_path(user))
+
+      fill_in 'First name', with: ""
+
+      click_on "Update Account"
+
+      expect(current_path).to eq(edit_user_path(user))
+      # expect(page).to have_content("Not valid information")
     end
   end
 

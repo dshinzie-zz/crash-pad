81beb996623470adc7d578c4b9ab1ab7cfd3deec
Brad <bgreen3@uoregon.edu>
2016-12-17 16:53:24 -0700

    Add endpoint for creating a review for a listing

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -7,6 +7,16 @@ class Api::V1::ReviewsController < ApplicationController
     end
   end
 
+  def create
+    if authenticated_user
+      listing = Listing.find(params[:listing_id])
+      review = listing.reviews.create(body: params[:body], user_id: authenticated_user.id)
+      render :json => review.to_json, :status => 201
+    else
+      render :json => {:error => 'forbidden'}.to_json, :status => 403
+    end
+  end
+
   private
 
   def authenticated_user
---
+++ spec/requests/api/v1/reviews_create_request_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe "reviews crud API" do
+  it "creates a review for authenticated user" do
+
+  user = create(:user)
+  listing = create(:listing)
+
+  params = {'body' => 'a new review',
+            'CONTENT_TYPE' => 'application/json',
+            'ACCEPT' => 'application/json'}
+
+  post "/api/v1/listings/#{listing.id}/reviews.json?api_key=#{user.api_key}", params: params
+
+  parsed = JSON.parse(response.body)
+  expect(response).to be_success
+  expect(parsed["body"]).to eq('a new review')
+  end
+  it "does not create a review for unauthenticated user" do
+
+  user = create(:user)
+  listing = create(:listing)
+
+  params = {'body' => 'a new review',
+            'CONTENT_TYPE' => 'application/json',
+            'ACCEPT' => 'application/json'}
+
+  post "/api/v1/listings/#{listing.id}/reviews.json?api_key=nottherightkey", params: params
+
+  parsed = JSON.parse(response.body)
+  expect(response.status).to be(403)
+  end
+end

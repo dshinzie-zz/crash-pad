2ae59af7d08dc654e4030c524a3ba38a8a667107
Daniel Shin <shin.daniel.d@gmail.com>
2016-12-17 13:00:41 -0700

    add blank sad test

--- spec/features/listings/user_can_create_new_listing_spec.rb
+++ spec/features/listings/user_can_create_new_listing_spec.rb
@@ -23,7 +23,7 @@ describe "As a logged in user" do
   end
 
   context "When I enter an invalid address" do
-    it "does not save to the database" do
+    it "does not save bad address to the database" do
       user = stub_login_user
 
       visit new_listing_path
@@ -39,6 +39,26 @@ describe "As a logged in user" do
 
       expect(current_path).to eq(new_listing_path)
       expect(page).to have_content("Please enter a valid address")
+      expect(Listing.count).to eq(0)
+    end
+
+    it "does not save blank address to the database" do
+      user = stub_login_user
+
+      visit new_listing_path
+
+      fill_in "City", with: ""
+      fill_in "State", with: ""
+      fill_in "Address", with: ""
+      fill_in "Description", with: "Test"
+      fill_in "Price", with: 99.00
+      fill_in "Accomodation", with: 2
+
+      click_button "Create Listing"
+
+      expect(current_path).to eq(new_listing_path)
+      expect(page).to have_content("Please enter a valid address")
+      expect(Listing.count).to eq(0)
     end
   end
 

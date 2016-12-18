5a1a6d445cf81989496f4f9b50d72c843ba1972d
Brad <bgreen3@uoregon.edu>
2016-12-18 11:33:04 -0700

    Set up test for admin listing show page

---
+++ spec/features/admin/admin_can_see_individual_listing_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "A logged in admin" do
+  context "who clicks on a listing" do
+    it "sees information for said listing" do
+      create_list :listing, 2
+      first_listing = Listing.first
+      second_listing = Listing.second
+
+      stub_admin_user
+
+      visit admin_listings_path
+
+      click_on "#{first_listing.id}"
+
+      expect(current_path).to eq(admin_listing_path(first_listing))
+      expect(page).to have_content(first_listing.id)
+      expect(page).to have_content(first_listing.user_id)
+      expect(page).to have_content(first_listing.description)
+      expect(page).to have_content(first_listing.price)
+      expect(page).to have_content(first_listing.accomodation)
+    end
+  end
+end

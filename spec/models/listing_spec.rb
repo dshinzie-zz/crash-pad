7ab7f0c10938bb7f3dcda960938a26ce0ca48500
Michael Scherer <michael.c.scherer@gmail.com>
2016-12-19 16:12:19 -0700

    Test it can create nights over a year

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -42,8 +42,14 @@ describe Listing do
         expect(listing.nights.last.date).to eq(Date.parse(listing.end_date))
         expect(listing.nights.count).to eq(3)
       end
-      it "creates" do
 
+      it "creates nights over a new year" do
+        listing = create(:listing, start_date: '30/12/2016', end_date: '1/1/2017')
+        listing.create_nights(listing.start_date, listing.end_date)
+
+        expect(listing.nights.first.date).to eq(Date.parse(listing.start_date))
+        expect(listing.nights.last.date).to eq(Date.parse(listing.end_date))
+        expect(listing.nights.count).to eq(3)
       end
     end
   end

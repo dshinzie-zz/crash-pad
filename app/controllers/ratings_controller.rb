c870f65672e73e1cfc50c7fe382550ca93d47017
Chase Dunagan <cdunagan@utexas.edu>
2016-12-19 17:02:12 -0700

    add tests for ratings model

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,7 +2,7 @@ class RatingsController < ApplicationController
   def create
     listing = Listing.find(params[:listing_id])
     # rating = listing.ratings.find(params[:id])
-    listing.ratings.find_or_create_by(rating_params)
+    listing.ratings.create(rating_params)
 
     redirect_to listing_path(listing)
   end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Rating, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:listing) }
 end

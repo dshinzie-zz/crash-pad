7e446d3526d76d72de15361687305a634a844431
Daniel Shin <shin.daniel.d@gmail.com>
2016-12-21 21:03:13 -0700

    Revert "23 filter by guests"

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
-require 'capybara/rspec'
 require 'vcr'
 require 'support/factory_girl'
 
@@ -68,7 +67,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and

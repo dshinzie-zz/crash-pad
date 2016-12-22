38f0585bba7e0026b25906e3b6a968d0f699a0da
Daniel Shin <shin.daniel.d@gmail.com>
2016-12-22 00:10:32 -0700

    fix all vcr conflicts

--- spec/features/users/guest_creates_account_spec.rb
+++ spec/features/users/guest_creates_account_spec.rb
@@ -1,18 +1,22 @@
 require 'rails_helper'
 
 describe "A not logged in user" do
-  scenario "creates an account" do
-    visit new_user_path
+  context "creates an account" do
+    it "takes me to verify path" do
+      VCR.use_cassette(".by_guest_account") do
+        visit new_user_path
 
-    fill_in 'Email', with: "johnnyquest@test.com"
-    fill_in 'Password', with: "password"
-    fill_in 'First Name', with: "Johnny"
-    fill_in 'Last Name', with: "Quest"
-    fill_in 'Phone Number', with: "999-999-9999"
+        fill_in 'Email', with: "johnnyquest@test.com"
+        fill_in 'Password', with: "password"
+        fill_in 'First Name', with: "Johnny"
+        fill_in 'Last Name', with: "Quest"
+        fill_in 'Phone Number', with: "999-999-9999"
 
-    click_button "Create Account"
+        click_button "Create Account"
 
-    expect(current_path).to eq(users_verify_path)
+        expect(current_path).to eq(users_verify_path)
+      end
+    end
   end
 
   scenario "returns to form if account not valid" do
--- spec/services/geocode_service_spec.rb
+++ spec/services/geocode_service_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 describe 'Geocode service' do
   context 'when I pass in a location' do
-    it "returns the results response", :vcr do
-      address = "1510 Blake St Denver CO"
-      geocode = GeocodeService.new.find_lat_long(address.parameterize)
+    it "returns the results response" do
+      VCR.use_cassette(".geocode") do
+        address = "1510 Blake St Denver CO"
+        geocode = GeocodeService.new.find_lat_long(address.parameterize)
 
-      expect(geocode.class).to eq(Hash)
-      expect(geocode).to have_key(:results)
+        expect(geocode.class).to eq(Hash)
+        expect(geocode).to have_key(:results)
+      end
     end
   end
 end

9480e6bc966fc527972ced7034ef88349529ffc3
Daniel Shin <shin.daniel.d@gmail.com>
2016-12-22 01:50:29 -0700

    fix form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,6 +5,7 @@ class BookingsController < ApplicationController
   def new
     @listing = Listing.find(params[:listing_id])
     @booking = Booking.new
+    @booked_nights = Night.booked_nights
   end
 
   def create
